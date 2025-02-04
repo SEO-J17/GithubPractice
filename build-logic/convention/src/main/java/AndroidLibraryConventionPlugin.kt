9c8a9eb15826f3b41b8a5544f470f4c9bc46d199
seojeongwoo <qw4643987@naver.com>
2025-02-04 15:56:36 +0900

    feat: 안드로이드 라이브러리 관련 커스텀 플러그인 구현

---
+++ build-logic/convention/src/main/java/AndroidLibraryConventionPlugin.kt
@@ -0,0 +1,28 @@
+import com.android.build.gradle.LibraryExtension
+import io.seoj17.convention.configureKotlinAndroid
+import io.seoj17.convention.libs
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
+
+class AndroidLibraryConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            with(pluginManager) {
+                apply("com.android.library")
+                apply("org.jetbrains.kotlin.android")
+                apply("org.jetbrains.kotlin.plugin.serialization")
+            }
+
+            extensions.configure<LibraryExtension> {
+                configureKotlinAndroid(this)
+                defaultConfig.targetSdk = 34
+            }
+
+            dependencies {
+                add("implementation", libs.findLibrary("kotlinx.serialization.json").get())
+            }
+        }
+    }
+}
