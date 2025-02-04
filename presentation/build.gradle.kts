7a1c7d140af61bb29b93d721a425734beb2956e4
seojeongwoo <qw4643987@naver.com>
2025-02-04 16:26:33 +0900

    feat: ui 모듈 생성

---
+++ presentation/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    alias(libs.plugins.seoj17.hilt)
+    alias(libs.plugins.seoj17.android.library)
+    alias(libs.plugins.seoj17.android.library.compose)
+}
+
+android {
+    namespace = "io.seoj17.soop.presentation"
+}
+
+dependencies {
+    implementation(project(":domain"))
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.appcompat)
+    // coroutine
+    implementation(libs.bundles.coroutine)
+    // hilt navigation compose
+    implementation(libs.hilt.navigation.compose)
+    // navigation compose
+    implementation(libs.navigation.compose)
+    // compose coil
+    implementation(libs.coil.compose)
+}
