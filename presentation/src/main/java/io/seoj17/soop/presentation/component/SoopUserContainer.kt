820e1c78a749f743e300ffa6b0f6b947f6c14893
seojeongwoo <qw4643987@naver.com>
2025-02-06 14:13:41 +0900

    feat: 유저 컨테이너 컴포넌트 구현

---
+++ presentation/src/main/java/io/seoj17/soop/presentation/component/SoopUserContainer.kt
@@ -0,0 +1,48 @@
+package io.seoj17.soop.presentation.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+
+@Composable
+fun SoopUserContainer(modifier: Modifier, userThumbnailUrl: String, userName: String) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.spacedBy(10.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        AsyncImage(
+            modifier = Modifier
+                .clip(shape = CircleShape)
+                .size(30.dp),
+            model = userThumbnailUrl,
+            contentScale = ContentScale.Fit,
+            contentDescription = "유저 썸네일"
+        )
+        Text(
+            text = userName,
+            color = Color.Gray
+        )
+    }
+}
+
+@Composable
+@Preview
+private fun SoopUserContainerPreview() {
+    SoopUserContainer(
+        modifier = Modifier,
+        userThumbnailUrl = "https://avatars.githubusercontent.com/u/48726953?v=4",
+        userName = "seoj17"
+    )
+}
