83b639493729199620cd62ae334a56247ff8631a
seojeongwoo <qw4643987@naver.com>
2025-02-06 21:22:47 +0900

    feat: searchDetail 화면 네비게이션 구현

---
+++ presentation/src/main/java/io/seoj17/soop/presentation/ui/detail/naviagation/SearchDetailNavigation.kt
@@ -0,0 +1,18 @@
+package io.seoj17.soop.presentation.ui.detail.naviagation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import io.seoj17.soop.presentation.navigation.SoopRoute
+
+fun NavController.navigateSearchDetail() {
+    navigate(
+        SoopRoute.SearchDetail.route,
+    )
+}
+
+fun NavGraphBuilder.searchDetailNavigation() {
+    composable(route = SoopRoute.SearchDetail.route) {
+        SearchDetailRoute()
+    }
+}
