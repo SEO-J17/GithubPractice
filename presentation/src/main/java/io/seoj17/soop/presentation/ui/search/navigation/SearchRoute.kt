ddab2a6cb7012dbe836088e15fe6680a682a6d51
seojeongwoo <qw4643987@naver.com>
2025-02-07 14:44:15 +0900

    feat: 검색 라우트 mvi 로직 구현

--- presentation/src/main/java/io/seoj17/soop/presentation/ui/search/navigation/SearchRoute.kt
+++ presentation/src/main/java/io/seoj17/soop/presentation/ui/search/navigation/SearchRoute.kt
@@ -1,15 +1,34 @@
 package io.seoj17.soop.presentation.ui.search.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import io.seoj17.soop.presentation.ui.search.SearchScreen
+import io.seoj17.soop.presentation.ui.search.SearchViewModel
+import io.seoj17.soop.presentation.ui.search.mvi.SearchIntent
+import io.seoj17.soop.presentation.ui.search.mvi.SearchSideEffect
 import io.seoj17.soop.presentation.utils.ImmutableList
 
 @Composable
-fun SearchRoute() {
+fun SearchRoute(onClickSearchResultItem: () -> Unit, viewModel: SearchViewModel = hiltViewModel()) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    LaunchedEffect(viewModel.sideEffect) {
+        viewModel.sideEffect.collect { sideEffect ->
+            when (sideEffect) {
+                is SearchSideEffect.NavigateToSearchDetail -> onClickSearchResultItem()
+            }
+        }
+    }
+
     SearchScreen(
         onClickSearch = { searchText ->
-
         },
         repoList = ImmutableList(emptyList()),
+        onClickSearchResultItem = {
+            viewModel.handleIntent(SearchIntent.ClickSearchResultItem)
+        },
     )
 }
