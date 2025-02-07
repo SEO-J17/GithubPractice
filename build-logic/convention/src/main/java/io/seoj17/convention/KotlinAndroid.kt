9991762e03e1bbe9791a90176be304172fad5b10
SEO JEONGWOO <59912150+SEO-J17@users.noreply.github.com>
2025-02-07 16:27:45 +0900

    [#24] Build 버그 수정 (#25)
    * feat: JvmToolChain 관련 코드 추가
    
    * chore: kotlinSerializationPlugin 버전 다운 그레이드

--- build-logic/convention/src/main/java/io/seoj17/convention/KotlinAndroid.kt
+++ build-logic/convention/src/main/java/io/seoj17/convention/KotlinAndroid.kt
@@ -4,7 +4,10 @@ import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.JavaVersion
 import org.gradle.api.Project
 import org.gradle.api.plugins.JavaPluginExtension
+import org.gradle.jvm.toolchain.JavaLanguageVersion
 import org.gradle.kotlin.dsl.configure
+import org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
 
 internal fun Project.configureKotlinAndroid(
     commonExtension: CommonExtension<*, *, *, *, *, *>,
@@ -22,11 +25,22 @@ internal fun Project.configureKotlinAndroid(
             isCoreLibraryDesugaringEnabled = false
         }
     }
+    extensions.configure<KotlinAndroidProjectExtension> {
+        jvmToolchain {
+            languageVersion.set(JavaLanguageVersion.of(JavaVersion.VERSION_17.toString()))
+        }
+    }
 }
 
 internal fun Project.configureKotlinJvm() {
     extensions.configure<JavaPluginExtension> {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    extensions.configure<KotlinJvmProjectExtension> {
+        jvmToolchain {
+            languageVersion.set(JavaLanguageVersion.of(JavaVersion.VERSION_17.toString()))
+        }
     }
 }
