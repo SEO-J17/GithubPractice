3a6063eb2f69f7f9f92081465b39ccd841230eeb
seojeongwoo <qw4643987@naver.com>
2025-02-07 14:45:17 +0900

    feat: 검색 상세 화면 라우트 mvi 구현

--- presentation/src/main/java/io/seoj17/soop/presentation/ui/detail/naviagation/SearchDetailRoute.kt
+++ presentation/src/main/java/io/seoj17/soop/presentation/ui/detail/naviagation/SearchDetailRoute.kt
@@ -1,15 +1,25 @@
 package io.seoj17.soop.presentation.ui.detail.naviagation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import io.seoj17.soop.presentation.ui.detail.SearchDetailScreen
+import io.seoj17.soop.presentation.ui.detail.SearchDetailViewModel
+import io.seoj17.soop.presentation.ui.detail.mvi.SearchDetailIntent
 
 @Composable
-fun SearchDetailRoute() {
+fun SearchDetailRoute(
+    viewModel: SearchDetailViewModel = hiltViewModel(),
+) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
     SearchDetailScreen(
         repoName = "",
         repoLanguage = "",
         userName = "",
         onClickUserDetail = {},
-        isBottomSheetVisible = true,
+        isBottomSheetVisible = uiState.isBottomSheetVisible,
+        onTouchBottomSheetClose = { viewModel.handleIntent(SearchDetailIntent.TouchBottomSheetClose) },
     )
 }
