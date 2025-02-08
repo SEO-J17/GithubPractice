61919de2fabf71a8142c31fd8e8229e2048afe99
seojeongwoo <qw4643987@naver.com>
2025-02-08 22:54:08 +0900

    feat: 검색 유스케이스 로직 구현

--- presentation/src/main/java/io/seoj17/soop/presentation/ui/search/SearchViewModel.kt
+++ presentation/src/main/java/io/seoj17/soop/presentation/ui/search/SearchViewModel.kt
@@ -1,16 +1,26 @@
 package io.seoj17.soop.presentation.ui.search
 
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.seoj17.soop.domain.usecase.GetRepoListUseCase
 import io.seoj17.soop.presentation.base.BaseViewModel
+import io.seoj17.soop.presentation.ui.search.model.toUiModel
 import io.seoj17.soop.presentation.ui.search.mvi.SearchIntent
 import io.seoj17.soop.presentation.ui.search.mvi.SearchSideEffect
 import io.seoj17.soop.presentation.ui.search.mvi.SearchUiState
+import io.seoj17.soop.presentation.utils.ImmutableList
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class SearchViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
+    private val getRepoListUseCase: GetRepoListUseCase,
+
 ) : BaseViewModel<SearchUiState, SearchSideEffect, SearchIntent>(savedStateHandle) {
     override fun createInitialState(savedStateHandle: SavedStateHandle): SearchUiState {
         return SearchUiState.initialize()
@@ -19,12 +29,32 @@ class SearchViewModel @Inject constructor(
     override fun handleIntent(intent: SearchIntent) {
         when (intent) {
             is SearchIntent.ClickSearch -> {
-                // TODO : Implement search logic
+                getRepoList(intent.searchText)
             }
 
             is SearchIntent.ClickSearchResultItem -> {
                 postSideEffect(SearchSideEffect.NavigateToSearchDetail)
             }
+        }
+    }
+
+    private fun updateLoading(isLoading: Boolean) {
+        reduce {
+            copy(isLoading = isLoading)
+        }
+    }
+
+    private fun getRepoList(repoName: String) {
+        viewModelScope.launch {
+            getRepoListUseCase(repoName)
+                .onStart { updateLoading(true) }
+                .onCompletion { updateLoading(false) }
+                .map { it.toUiModel() }
+                .collect { repoList ->
+                    reduce {
+                        copy(repoList = ImmutableList(repoList))
+                    }
+                }
         }
     }
 }
