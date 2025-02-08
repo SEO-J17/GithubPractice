8b1906ec24962a3433381b181678c6559fa3d38b
seojeongwoo <qw4643987@naver.com>
2025-02-08 22:56:06 +0900

    feat: state에 실제 데이터 추가

--- presentation/src/main/java/io/seoj17/soop/presentation/ui/search/mvi/SearchUiState.kt
+++ presentation/src/main/java/io/seoj17/soop/presentation/ui/search/mvi/SearchUiState.kt
@@ -1,11 +1,17 @@
 package io.seoj17.soop.presentation.ui.search.mvi
 
 import io.seoj17.soop.presentation.base.UiState
+import io.seoj17.soop.presentation.ui.search.model.RepoInfo
+import io.seoj17.soop.presentation.utils.ImmutableList
 
 data class SearchUiState(
     val isLoading: Boolean,
+    val repoList: ImmutableList<RepoInfo>,
 ) : UiState {
     companion object {
-        fun initialize() = SearchUiState(isLoading = false)
+        fun initialize() = SearchUiState(
+            isLoading = false,
+            repoList = ImmutableList(emptyList()),
+        )
     }
 }
