7d1ca598563acb200df1d5437f2dd49df9e06e24
seojeongwoo <qw4643987@naver.com>
2025-02-08 17:19:59 +0900

    feat: repo 상세정보 get 함수 repository 구현

--- data/src/main/java/io/seoj17/soop/data/repository/RepoRepository.kt
+++ data/src/main/java/io/seoj17/soop/data/repository/RepoRepository.kt
@@ -1,7 +1,9 @@
 package io.seoj17.soop.data.repository
 
+import io.seoj17.soop.data.model.RepoDetailDataModel
 import io.seoj17.soop.data.model.RepoInfoDataModel
 
 interface RepoRepository {
     suspend fun getRepoList(repoName: String): List<RepoInfoDataModel>
+    suspend fun getRepoDetail(userName: String, repoName: String): RepoDetailDataModel
 }
--- data/src/main/java/io/seoj17/soop/data/repository/RepoRepositoryImpl.kt
+++ data/src/main/java/io/seoj17/soop/data/repository/RepoRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package io.seoj17.soop.data.repository
 
 import io.seoj17.soop.data.datasource.RepoDataSource
+import io.seoj17.soop.data.model.RepoDetailDataModel
 import io.seoj17.soop.data.model.RepoInfoDataModel
 import javax.inject.Inject
 
@@ -9,5 +10,9 @@ class RepoRepositoryImpl @Inject constructor(
 ) : RepoRepository {
     override suspend fun getRepoList(repoName: String): List<RepoInfoDataModel> {
         return repoDataSource.getRepoList(repoName)
+    }
+
+    override suspend fun getRepoDetail(userName: String, repoName: String): RepoDetailDataModel {
+        return repoDataSource.getRepoDetail(userName, repoName)
     }
 }
