7097e3b7d9c5e82e4e57a430d227f8ab39882280
seojeongwoo <qw4643987@naver.com>
2025-02-08 17:19:15 +0900

    feat: repo detail 서비스 구현

--- data/src/main/java/io/seoj17/soop/data/service/GithubRepoService.kt
+++ data/src/main/java/io/seoj17/soop/data/service/GithubRepoService.kt
@@ -1,7 +1,9 @@
 package io.seoj17.soop.data.service
 
 import io.seoj17.soop.data.response.GithubRepoResponse
+import io.seoj17.soop.data.response.RepoDetailResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface GithubRepoService {
@@ -9,4 +11,10 @@ interface GithubRepoService {
     suspend fun getRepoList(
         @Query(value = "q") repoName: String,
     ): GithubRepoResponse
+
+    @GET("repos/{owner}/{repo}")
+    suspend fun getUserRepoDetail(
+        @Path(value = "owner") userName: String,
+        @Path(value = "repo") repoName: String,
+    ): RepoDetailResponse
 }
