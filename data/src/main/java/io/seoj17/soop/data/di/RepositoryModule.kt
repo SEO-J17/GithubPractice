29f4615e59017ce6acae5036d07f85f91e867d76
seojeongwoo <qw4643987@naver.com>
2025-02-08 21:30:09 +0900

    feat: UserRepository bind 로직 추가

--- data/src/main/java/io/seoj17/soop/data/di/RepositoryModule.kt
+++ data/src/main/java/io/seoj17/soop/data/di/RepositoryModule.kt
@@ -6,6 +6,8 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import io.seoj17.soop.data.repository.RepoRepository
 import io.seoj17.soop.data.repository.RepoRepositoryImpl
+import io.seoj17.soop.data.repository.UserRepository
+import io.seoj17.soop.data.repository.UserRepositoryImpl
 import javax.inject.Singleton
 
 @Module
@@ -15,4 +17,8 @@ interface RepositoryModule {
     @Binds
     @Singleton
     fun bindRepoRepository(repoRepositoryImpl: RepoRepositoryImpl): RepoRepository
+
+    @Binds
+    @Singleton
+    fun bindUserRepository(userRepositoryImpl: UserRepositoryImpl): UserRepository
 }
