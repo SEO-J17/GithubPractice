fb354143084e84aa755d95d51d36009292676735
seojeongwoo <qw4643987@naver.com>
2025-02-08 15:39:24 +0900

    feat: 데이터소스 구현

---
+++ data/src/main/java/io/seoj17/soop/data/datasource/RepoDataSourceImpl.kt
@@ -0,0 +1,14 @@
+package io.seoj17.soop.data.datasource
+
+import io.seoj17.soop.data.model.RepoInfoDataModel
+import io.seoj17.soop.data.model.toDataModel
+import io.seoj17.soop.data.service.GithubRepoService
+import javax.inject.Inject
+
+class RepoDataSourceImpl @Inject constructor(
+    private val githubRepoService: GithubRepoService,
+) : RepoDataSource {
+    override suspend fun getRepoList(repoName: String): List<RepoInfoDataModel> {
+        return githubRepoService.getRepoList(repoName).items.toDataModel()
+    }
+}
