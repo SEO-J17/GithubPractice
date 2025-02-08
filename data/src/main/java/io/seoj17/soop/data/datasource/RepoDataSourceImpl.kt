62688637abd059be7f2ccaf0dea89de0132e5ce7
seojeongwoo <qw4643987@naver.com>
2025-02-08 21:31:19 +0900

    feat: User repoList get하는 함수 추가

--- data/src/main/java/io/seoj17/soop/data/datasource/RepoDataSourceImpl.kt
+++ data/src/main/java/io/seoj17/soop/data/datasource/RepoDataSourceImpl.kt
@@ -2,12 +2,15 @@ package io.seoj17.soop.data.datasource
 
 import io.seoj17.soop.data.model.RepoDetailDataModel
 import io.seoj17.soop.data.model.RepoInfoDataModel
+import io.seoj17.soop.data.model.UserRepoDataModel
 import io.seoj17.soop.data.model.toDataModel
 import io.seoj17.soop.data.service.GithubRepoService
+import io.seoj17.soop.data.service.UserService
 import javax.inject.Inject
 
 class RepoDataSourceImpl @Inject constructor(
     private val githubRepoService: GithubRepoService,
+    private val userService: UserService,
 ) : RepoDataSource {
     override suspend fun getRepoList(repoName: String): List<RepoInfoDataModel> {
         return githubRepoService.getRepoList(repoName).items.toDataModel()
@@ -15,5 +18,9 @@ class RepoDataSourceImpl @Inject constructor(
 
     override suspend fun getRepoDetail(userName: String, repoName: String): RepoDetailDataModel {
         return githubRepoService.getUserRepoDetail(userName, repoName).toDataModel()
+    }
+
+    override suspend fun getUserRepoList(userName: String): List<UserRepoDataModel> {
+        return userService.getUserRepoList(userName).toDataModel()
     }
 }
--- data/src/main/java/io/seoj17/soop/data/repository/RepoRepository.kt
+++ data/src/main/java/io/seoj17/soop/data/repository/RepoRepository.kt
@@ -2,8 +2,10 @@ package io.seoj17.soop.data.repository
 
 import io.seoj17.soop.data.model.RepoDetailDataModel
 import io.seoj17.soop.data.model.RepoInfoDataModel
+import io.seoj17.soop.data.model.UserRepoDataModel
 
 interface RepoRepository {
     suspend fun getRepoList(repoName: String): List<RepoInfoDataModel>
     suspend fun getRepoDetail(userName: String, repoName: String): RepoDetailDataModel
+    suspend fun getUserRepoList(userName: String): List<UserRepoDataModel>
 }
