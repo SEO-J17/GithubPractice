abc303849513618500f5efbc44288315eaa7e553
seojeongwoo <qw4643987@naver.com>
2025-02-09 14:30:57 +0900

    feat: 페이저 데이터로 구현

--- data/src/main/java/io/seoj17/soop/data/repository/RepoRepository.kt
+++ data/src/main/java/io/seoj17/soop/data/repository/RepoRepository.kt
@@ -1,11 +1,12 @@
 package io.seoj17.soop.data.repository
 
+import androidx.paging.Pager
 import io.seoj17.soop.data.model.RepoDetailDataModel
 import io.seoj17.soop.data.model.RepoInfoDataModel
 import io.seoj17.soop.data.model.UserRepoDataModel
 
 interface RepoRepository {
-    suspend fun getRepoList(repoName: String): List<RepoInfoDataModel>
+    fun getRepoList(repoName: String): Pager<Int, RepoInfoDataModel>
     suspend fun getRepoDetail(userName: String, repoName: String): RepoDetailDataModel
     suspend fun getUserRepoList(userName: String): List<UserRepoDataModel>
 }
--- data/src/main/java/io/seoj17/soop/data/repository/RepoRepositoryImpl.kt
+++ data/src/main/java/io/seoj17/soop/data/repository/RepoRepositoryImpl.kt
@@ -1,6 +1,9 @@
 package io.seoj17.soop.data.repository
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
 import io.seoj17.soop.data.datasource.RepoDataSource
+import io.seoj17.soop.data.datasource.RepoPagingSource
 import io.seoj17.soop.data.model.RepoDetailDataModel
 import io.seoj17.soop.data.model.RepoInfoDataModel
 import io.seoj17.soop.data.model.UserRepoDataModel
@@ -9,8 +12,17 @@ import javax.inject.Inject
 class RepoRepositoryImpl @Inject constructor(
     private val repoDataSource: RepoDataSource,
 ) : RepoRepository {
-    override suspend fun getRepoList(repoName: String): List<RepoInfoDataModel> {
-        return repoDataSource.getRepoList(repoName)
+    override fun getRepoList(repoName: String): Pager<Int, RepoInfoDataModel> {
+        return Pager(
+            config = PagingConfig(
+                pageSize = NETWORK_PAGE_SIZE,
+                enablePlaceholders = false,
+                initialLoadSize = NETWORK_PAGE_SIZE,
+            ),
+            pagingSourceFactory = {
+                RepoPagingSource(repoDataSource = repoDataSource, repoName = repoName)
+            },
+        )
     }
 
     override suspend fun getRepoDetail(userName: String, repoName: String): RepoDetailDataModel {
@@ -19,5 +31,9 @@ class RepoRepositoryImpl @Inject constructor(
 
     override suspend fun getUserRepoList(userName: String): List<UserRepoDataModel> {
         return repoDataSource.getUserRepoList(userName)
+    }
+
+    companion object {
+        private const val NETWORK_PAGE_SIZE = 30
     }
 }
