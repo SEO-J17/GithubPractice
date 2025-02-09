3dbb0467b24a073673abb44d38c75c0d56537e82
seojeongwoo <qw4643987@naver.com>
2025-02-09 15:06:06 +0900

    feat: 에러 토스트 띄우는 로직 추가

--- presentation/src/main/java/io/seoj17/soop/presentation/ui/detail/naviagation/SearchDetailRoute.kt
+++ presentation/src/main/java/io/seoj17/soop/presentation/ui/detail/naviagation/SearchDetailRoute.kt
@@ -1,18 +1,39 @@
 package io.seoj17.soop.presentation.ui.detail.naviagation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import io.seoj17.soop.presentation.R
 import io.seoj17.soop.presentation.ui.detail.SearchDetailScreen
 import io.seoj17.soop.presentation.ui.detail.SearchDetailViewModel
 import io.seoj17.soop.presentation.ui.detail.mvi.SearchDetailIntent
+import io.seoj17.soop.presentation.ui.detail.mvi.SearchDetailSideEffect
+import io.seoj17.soop.presentation.utils.showToast
 
 @Composable
 fun SearchDetailRoute(
+    onBackPreviousScreen: () -> Unit,
     viewModel: SearchDetailViewModel = hiltViewModel(),
 ) {
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val context = LocalContext.current
+
+    LaunchedEffect(viewModel.sideEffect) {
+        viewModel.sideEffect.collect { sideEffect ->
+            when (sideEffect) {
+                is SearchDetailSideEffect.BackPreviousScreen -> {
+                    onBackPreviousScreen()
+                }
+
+                SearchDetailSideEffect.ShowError -> {
+                    context.showToast(R.string.error_message)
+                }
+            }
+        }
+    }
 
     SearchDetailScreen(
         uiState = uiState,
--- presentation/src/main/java/io/seoj17/soop/presentation/ui/search/navigation/SearchRoute.kt
+++ presentation/src/main/java/io/seoj17/soop/presentation/ui/search/navigation/SearchRoute.kt
@@ -3,12 +3,15 @@ package io.seoj17.soop.presentation.ui.search.navigation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import io.seoj17.soop.presentation.R
 import io.seoj17.soop.presentation.ui.search.SearchScreen
 import io.seoj17.soop.presentation.ui.search.SearchViewModel
 import io.seoj17.soop.presentation.ui.search.mvi.SearchIntent
 import io.seoj17.soop.presentation.ui.search.mvi.SearchSideEffect
+import io.seoj17.soop.presentation.utils.showToast
 
 @Composable
 fun SearchRoute(
@@ -16,6 +19,7 @@ fun SearchRoute(
     viewModel: SearchViewModel = hiltViewModel(),
 ) {
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val context = LocalContext.current
 
     LaunchedEffect(viewModel.sideEffect) {
         viewModel.sideEffect.collect { sideEffect ->
@@ -24,6 +28,8 @@ fun SearchRoute(
                     sideEffect.userName,
                     sideEffect.repoName,
                 )
+
+                SearchSideEffect.ShowError -> context.showToast(R.string.error_message)
             }
         }
     }
