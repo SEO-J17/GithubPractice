95627b11f0e4c589dba68b59f2e66dec76e27094
seojeongwoo <qw4643987@naver.com>
2025-02-09 14:30:05 +0900

    feat: 페이징 소스 구현

---
+++ data/src/main/java/io/seoj17/soop/data/datasource/RepoPagingSource.kt
@@ -0,0 +1,40 @@
+package io.seoj17.soop.data.datasource
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import io.seoj17.soop.data.model.RepoInfoDataModel
+
+class RepoPagingSource(
+    private val repoDataSource: RepoDataSource,
+    private val repoName: String,
+) : PagingSource<Int, RepoInfoDataModel>() {
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, RepoInfoDataModel> {
+        val page = params.key ?: START_PAGE
+
+        return try {
+            val repoInfoList = repoDataSource.getRepoList(
+                repoName = repoName,
+                page = page,
+                perPage = params.loadSize,
+            )
+            LoadResult.Page(
+                data = repoInfoList,
+                prevKey = if (page == START_PAGE) null else page - 1,
+                nextKey = if (repoInfoList.size < params.loadSize) null else page + 1,
+            )
+        } catch (e: Exception) {
+            LoadResult.Error(e)
+        }
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, RepoInfoDataModel>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            state.closestPageToPosition(anchorPosition)?.prevKey?.plus(1)
+                ?: state.closestPageToPosition(anchorPosition)?.nextKey?.minus(1)
+        }
+    }
+
+    companion object {
+        private const val START_PAGE = 1
+    }
+}
