0a63822eb49ea3f28d97e7785670c14a84273591
seojeongwoo <qw4643987@naver.com>
2025-02-09 22:11:45 +0900

    feat: 검색 결과가 없을때 sideEffect추가

--- presentation/src/main/java/io/seoj17/soop/presentation/ui/search/mvi/SearchSideEffect.kt
+++ presentation/src/main/java/io/seoj17/soop/presentation/ui/search/mvi/SearchSideEffect.kt
@@ -5,4 +5,5 @@ import io.seoj17.soop.presentation.base.UiSideEffect
 sealed interface SearchSideEffect : UiSideEffect {
     data class NavigateToSearchDetail(val userName: String, val repoName: String) : SearchSideEffect
     data object ShowError : SearchSideEffect
+    data object ShowEmptySearchResult : SearchSideEffect
 }
